6d79a01a8e6b29f7e82e43ec8115d4a8cb6517ab
yzhang3 <yzhang3@linkernetworks.com>
2016-03-09 09:37:21 +0800

    add submodule docker-machine

---
+++ docker-machine/create-machines.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+# This script can create and boot rancheros(s) in virtualbox
+# Usage:
+# create 5 machines
+# #./create-machines.sh 5
+
+
+## create vm powered by virtualbox
+##
+# iso: path to iso
+# mem: memory size(in MB)
+# name: name for docker machine
+##
+# return 0 if success
+function create_vm(){
+	local iso=$1
+	local mem=$2
+	local name=$3
+	
+	echo "start to create machine..."
+	echo "iso: $iso"
+	echo "mem: $mem MB"
+	echo "name: $name"
+	
+	docker-machine create -d virtualbox --virtualbox-boot2docker-url=$iso --virtualbox-memory=$mem $name
+}
+
+## copy public ssh key to machine by docker-machine scp
+# The key a newly generated ssh key and will be copied to all
+# rancheros VMs.
+function copy_key(){
+	local pubkey_path=$1
+	local vm_name=$2
+	local saving_path=$3
+	docker-machine scp $pubkey_path $vm_name:$saving_path
+}
+
+## append text key file
+# append key2 content to key1 tail
+function append_key(){
+	local vm_name=$1
+	local key1=$2
+	local key2=$3
+	echo "append key: $key2 to $key1"
+	docker-machine ssh $vm_name "cat $key2 >> $key1"
+}
+
+## delete file
+function delete(){
+	local vm_name=$1
+	local file=$2
+	docker-machine ssh $vm_name "rm $file"
+}
+
+echo "machine number: $1"
+machine_number=$1
+
+# CONFIGURATIONS AND SETUPS
+# config iso path and memory size
+iso=/root/school/project/docker-machine/rancheros.iso
+mem=512
+# config ssh key path and saving path
+pubkey_path=$PWD/.ssh/id_rsa.pub
+saving_path=/home/docker/.ssh/authorized_keys2
+authorized_keys=/home/docker/.ssh/authorized_keys
+
+for n in `seq $machine_number`
+do
+	printf "\n"
+	printf "[%02d/%02d] call docker machine to create vm \n" $n $machine_number
+	# CREATE AND BOOT MACHINES
+	name=$(printf "rancheros-%02d" $n)	
+	create_vm $iso $mem $name
+
+	printf "\n extra configurations begin...\n"
+	
+	# COPY MY SSH KEY
+	printf "copy my united public ssh key file...\n"
+	copy_key $pubkey_path $name $saving_path
+
+	# APPEND KEY
+	printf "append key..."
+	append_key $name $authorized_keys $saving_path
+
+	# DELETE KEY
+	printf "delete key2..."
+	delete $name $saving_path
+
+	printf "\n memory status:\n"
+	free -h
+done
+
