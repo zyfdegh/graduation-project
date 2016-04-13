ec58fb0da2995b1777be1e7b7ef8c499c7a19738
yzhang3 <yzhang3@linkernetworks.com>
2016-04-13 18:55:13 +0800

    check err after each step in build-all.sh

--- dockerfiles/build-all.sh
+++ dockerfiles/build-all.sh
@@ -7,18 +7,30 @@ echo -e "\n================\n"
 echo "Building centos7..."
 cd $DOCKERFILES/centos7/
 ./build.sh
+if [[ $? -ne 0 ]]; then 
+	 echo "build failed"
+	 exit 1
+fi
 
 echo -e "\n================\n"
 
 echo "Building jre8..."
 cd $DOCKERFILES/jre8/
 ./build.sh
+if [[ $? -ne 0 ]]; then 
+	 echo "build failed"
+	 exit 1
+fi
 
 echo -e "\n================\n"
 
 echo "Building gerrit..."
 cd $DOCKERFILES/gerrit/
 ./build.sh
+if [[ $? -ne 0 ]]; then 
+	 echo "build failed"
+	 exit 1
+fi
 
 echo -e "\n================\n"
 
@@ -31,33 +43,57 @@ echo -e "\n================\n"
 echo "Building haproxy..."
 cd $DOCKERFILES/haproxy/
 ./build.sh
+if [[ $? -ne 0 ]]; then 
+	 echo "build failed"
+	 exit 1
+fi
 
 echo -e "\n================\n"
 
 echo "Building marathon..."
 cd $DOCKERFILES/marathon/
 ./build.sh
+if [[ $? -ne 0 ]]; then 
+	 echo "build failed"
+	 exit 1
+fi
 
 echo -e "\n================\n"
 
 echo "Building mesos-master..."
 cd $DOCKERFILES/mesos-master/
 ./build.sh
+if [[ $? -ne 0 ]]; then 
+	 echo "build failed"
+	 exit 1
+fi
 
 echo -e "\n================\n"
 
 echo "Building mesos-slave..."
 cd $DOCKERFILES/mesos-slave/
 ./build.sh
+if [[ $? -ne 0 ]]; then 
+	 echo "build failed"
+	 exit 1
+fi
 
 echo -e "\n================\n"
 
 echo "Building nginx..."
 cd $DOCKERFILES/nginx/
 ./build.sh
+if [[ $? -ne 0 ]]; then 
+	 echo "build failed"
+	 exit 1
+fi
 
 echo -e "\n================\n"
 
 echo "Building zookeeper..."
 cd $DOCKERFILES/zookeeper/
 ./build.sh
+if [[ $? -ne 0 ]]; then 
+	 echo "build failed"
+	 exit 1
+fi
